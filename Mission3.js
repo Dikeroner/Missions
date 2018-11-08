1371d8a7ceb3a9aa86ca3c916e604e3f2ad474eb
Dikeroner <jordi.reyes@outlook.com>
2018-11-08 19:37:36 +0100

    fixeando cosas/comentarios

--- Mission3.js
+++ Mission3.js
@@ -9,7 +9,7 @@ let numero = 0;
 
 rl.question('Ingresa un número: ', (respuesta) => {
     numero = respuesta;
-    
+    // Comprobamos si el número introducido no es primo.
     for (let i = 2; i < numero; i++){
         if ( numero % i === 0){
             console.log(numero + ' No es un numero primo.')
@@ -17,14 +17,13 @@ rl.question('Ingresa un número: ', (respuesta) => {
         }
         
     };
-
-    if (numero === 1) {
-        console.log('El número 1 no puede primo.')
+    // Comprobamos si el número introducido es "1".
+    if (numero == 1) {
+        console.log('El número 1 no puede ser primo.')
     } else{
         console.log(numero + ' Si es un numero primo.')
     }
     
-
     rl.close();
 });
 
--- Mission4.js
+++ Mission4.js
@@ -13,7 +13,7 @@ let primosNoUltimo = [];
 
 rl.question('Ingresa un número: ', (respuesta) => {
     numero = respuesta;
-
+    // Creamos una función para saber si el número introducido es primo o no.
     function Primo(numero){
         if (numero < 0) {
             return false;
@@ -29,7 +29,7 @@ rl.question('Ingresa un número: ', (respuesta) => {
         }
     
     }
-
+        // Nos aseguramos de que el número introducido sea superior a 10
         if (numero < 11) {
           console.log('Error, ingresa un numero mayor que 10');
         } else{
@@ -38,9 +38,10 @@ rl.question('Ingresa un número: ', (respuesta) => {
                     primos.push(j);
                 } 
             }
+            // Guardamos todos los números menos el último en una array a parte
             primosNoUltimo = primos.slice(0, -1);
+            // Enseñamos la array que contiene todos los números menos el último y después mostramos el último número.
             console.log(`Los numeros primos de ${numero} son ${primosNoUltimo} y ${primos[primos.length - 1]}`);
-            // ${primos.forEach(function(primo, indice, array) {if primos})}`);
         }   
     
 
